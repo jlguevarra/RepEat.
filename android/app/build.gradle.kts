a66017b5c6a6ee2ab6edb803651a1097188e94c1
John Lloyd Guevarra <johnlloydguevarra0405@gmail.com>
2025-10-06 18:39:03 +0800

    jl

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 
 
 android {
-    namespace = "com.codecrusaders.rep_eat"
+    namespace = "com.example.rep_eat"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.rep_eat"
+        applicationId = "com.codecrusaders.rep_eat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
